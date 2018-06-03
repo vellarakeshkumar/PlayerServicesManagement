a34a77850b4ed8d86269c984f5f87edda769ef18
Rakesh <vellarakeshkumar@gmail.com>
2018-06-03 12:57:13 -0300

    PSM Second commit

---
+++ src/main/java/org/igt/PlayerServicesManagement/config/PSMDatabaseConfig.java
@@ -0,0 +1,12 @@
+package org.igt.PlayerServicesManagement.config;
+
+public class PSMDatabaseConfig {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("Welcome");
+
+	}
+
+}
